068a691f3885213cedfc6d64ff1f4df20e8b4583
AbdullahBe11 <aboudiberro33@gmail.com>
2025-12-31 17:21:53 +0200

    Add files via upload

--- backend/upload_db.js
+++ backend/upload_db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-// 🔄 WE SWITCHED TO 'createPool' TO FIX THE CRASH
+
 const db = mysql.createPool({
     host: "mysql-245f1ff5-coffee-shop-online-ordering.h.aivencloud.com",
     user: "avnadmin",
@@ -9,11 +9,11 @@ const db = mysql.createPool({
     port: 10608,
     ssl: { rejectUnauthorized: false },
     waitForConnections: true,
-    connectionLimit: 10, // Allows 10 users at once without crashing
+    connectionLimit: 10, 
     queueLimit: 0,
     multipleStatements: true
 });
 
-// Using a pool means you DO NOT need "db.connect()". It connects automatically.
+
 
 module.exports = db;
